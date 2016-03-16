3e5c72fce1a2455581f6d63bfa2248f935afd9dc
Mingu Chu <mingu@Mingus-MacBook-Pro.local>
2016-03-16 15:59:52 -0500

    alright

--- TwitterizerX/ViewController.m
+++ TwitterizerX/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UILabel *labelOutlet;
 
@@ -24,18 +24,33 @@
 - (IBAction)twitterize:(id)sender {
     NSString *textViewString = self.textView.text;
     NSMutableString *stringWithoutVowel = [NSMutableString stringWithString:textViewString];
-
+    
     NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
     for (int i = 0; i < [vowels count]; i++) {
         NSString *vowel = vowels[i];
         [stringWithoutVowel replaceOccurrencesOfString:vowel
-                                   withString:@""
-                                      options:NSCaseInsensitiveSearch
-                                        range:NSMakeRange(0, stringWithoutVowel.length)];
+                                            withString:@""
+                                               options:NSCaseInsensitiveSearch
+                                                 range:NSMakeRange(0, stringWithoutVowel.length)];
     }
-
+    
     self.textView.text = stringWithoutVowel;
+    [self textViewDidChange:self.textView];
 }
 
+-(void)textViewDidChange:(UITextView *)textView {
+    long counter = textView.text.length;
+    self.labelOutlet.text = [NSString stringWithFormat:@"%li", counter];
+}
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    
+    if ([textView.text length] == 0) {
+        return YES;
+    } else ([textView.text length] > 139); {
+        return NO;
+    }
+    
+}
 
 @end
