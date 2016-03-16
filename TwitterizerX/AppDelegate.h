52af7d9fd65db9955ec7cf210493d14492c464c2
Brandon Gress <WorkCompXD@brandons-macbook-pro.local>
2016-03-16 14:12:19 -0500

    Initial Commit

---
+++ TwitterizerX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterizerX
+//
+//  Created by Brandon Gress on 3/16/16.
+//  Copyright © 2016 Brandon Gress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterizerX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterizerX
+//
+//  Created by Brandon Gress on 3/16/16.
+//  Copyright © 2016 Brandon Gress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
